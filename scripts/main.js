e2a6b41a9e285a66412174126787fbeb43cc0e06
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-06 16:56:38 -0700

    removes borders for cleaner gameboard

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
-console.log("works");
+$(document).ready( function () {
+	$clickCounter = 0;
+	$x = "X";
+	$o = "O";
+
+	console.log("works");
+	
+	$('.box').click( function () {
+		$(this).html('X');
+		if (click%2 == 0) {
+
+		}
+	});
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
