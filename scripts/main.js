68eedbeeb37bd6d86556a9547ea0dd64d24ef703
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-06 20:54:14 -0700

    final

--- scripts/main.js
+++ scripts/main.js
@@ -5,45 +5,85 @@ $(document).ready( function () {
 
 	$finalScore = 0;
 
+	$boardArray = [];
+	// $('.box').map( function () {
+	// 	$boardArray = 
+	// });
+	console.log($('.box'));
+	console.log($boardArray);
+
 	console.log("works");
 
 	$('.box').click( function () {
 		if ($clickCounter % 2 == 0) {
 			$(this).html($x);
-			$finalScore = $finalScore + 1;
-			console.log($finalScore);
+
+			if (
+				($('#box1').html() == $x && $('#box2').html() == $x && $('#box3').html() == $x) ||
+				($('#box4').html() == $x && $('#box5').html() == $x && $('#box6').html() == $x) ||
+				($('#box7').html() == $x && $('#box8').html() == $x && $('#box9').html() == $x) ||
+
+				($('#box1').html() == $x && $('#box4').html() == $x && $('#box7').html() == $x) ||
+				($('#box2').html() == $x && $('#box5').html() == $x && $('#box8').html() == $x) ||
+				($('#box3').html() == $x && $('#box6').html() == $x && $('#box6').html() == $x) ||
+
+				($('#box1').html() == $x && $('#box5').html() == $x && $('#box9').html() == $x) ||
+				($('#box3').html() == $x && $('#box5').html() == $x && $('#box7').html() == $x) 
+				) {
+					alert('Player 1 Wins!');	
+				}
+
+				if ($('#box1').html() !== "" && $('#box2').html() !== "" && $('#box3').html() !== "" &&
+					$('#box4').html() !== "" && $('#box5').html() !== "" && $('#box6').html() !== "" &&
+					$('#box7').html() !== "" && $('#box8').html() !== "" && $('#box9').html() !== "") 
+					 {
+					alert('Ya Both Win!');
+				}
+				
+
 			$clickCounter = $clickCounter + 1;
 			$('.playerTurn').html('Player 2 Go!');
-		} else  {
+
+
+		}  else {
 			$(this).html($o);
+			if (
+				($('#box1').html() == $o && $('#box2').html() == $o && $('#box3').html() == $o) ||
+				($('#box4').html() == $o && $('#box5').html() == $o && $('#box6').html() == $o) ||
+				($('#box7').html() == $o && $('#box8').html() == $o && $('#box9').html() == $o) ||
+
+				($('#box1').html() == $o && $('#box4').html() == $o && $('#box7').html() == $o) ||
+				($('#box2').html() == $o && $('#box5').html() == $o && $('#box8').html() == $o) ||
+				($('#box3').html() == $o && $('#box6').html() == $o && $('#box6').html() == $o) ||
+
+				($('#box1').html() == $o && $('#box5').html() == $o && $('#box9').html() == $o) ||
+				($('#box3').html() == $o && $('#box5').html() == $o && $('#box7').html() == $o) 
+				) {
+				alert('Player 2 Wins!');	
+				}
+
+				if ($('#box1').html() !== "" && $('#box2').html() !== "" && $('#box3').html() !== "" &&
+					$('#box4').html() !== "" && $('#box5').html() !== "" && $('#box6').html() !== "" &&
+					$('#box7').html() !== "" && $('#box8').html() !== "" && $('#box9').html() !== "") 
+					 {
+					alert('Ya Both Win!');
+				} 
+
 			$clickCounter = $clickCounter + 1;
-			$finalScore = $finalScore - 1;
-			console.log($finalScore);
 			$('.playerTurn').html('Player 1 Go!');
 		}
 
-		if ($finalScore = 0) {
-			alert('Player 2 Wins MothaFucka!');
-		} else {
-			alert('Player 1 Wins... Owned!');
-		};
+	
+		});
 		
 	$(".reset").click( function() {
 		$('.box').html('');
 		$('.playerTurn').html('Player 1 Go!');
+		$clickCounter = 0;
+		
+
+
 	});
-
-	});
-
-
-
-
-
-
-
-
-
-
 
 });
 
