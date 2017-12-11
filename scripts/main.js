9ec2769b017ddf33910e52dd1df8ccbc49e2f544
Mark Rosenberg <mrk.rosenberg@gmail.com>
2017-12-11 08:37:44 -0700

    prevents clicking boxes twice

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,30 @@
 $(document).ready( function () {
-	$clickCounter = 0;
-	$x = "X";
-	$o = "O";
+	var clickCounter = 0;
+	var x = "X";
+	var o = "O";
 
-	$finalScore = 0;
+	var finalScore = 0;
 
-	$boardArray = [];
-	// $('.box').map( function () {
-	// 	$boardArray = 
-	// });
-	console.log($('.box'));
-	console.log($boardArray);
+
 
 	console.log("works");
 
 	$('.box').click( function () {
-		if ($clickCounter % 2 == 0) {
-			$(this).html($x);
+		if (clickCounter % 2 == 0 && !$(this).hasClass('O')) {
+			$(this).html(x);
+			$(this).addClass('X');
 
 			if (
-				($('#box1').html() == $x && $('#box2').html() == $x && $('#box3').html() == $x) ||
-				($('#box4').html() == $x && $('#box5').html() == $x && $('#box6').html() == $x) ||
-				($('#box7').html() == $x && $('#box8').html() == $x && $('#box9').html() == $x) ||
+				($('#box1').html() == x && $('#box2').html() == x && $('#box3').html() == x) ||
+				($('#box4').html() == x && $('#box5').html() == x && $('#box6').html() == x) ||
+				($('#box7').html() == x && $('#box8').html() == x && $('#box9').html() == x) ||
 
-				($('#box1').html() == $x && $('#box4').html() == $x && $('#box7').html() == $x) ||
-				($('#box2').html() == $x && $('#box5').html() == $x && $('#box8').html() == $x) ||
-				($('#box3').html() == $x && $('#box6').html() == $x && $('#box6').html() == $x) ||
+				($('#box1').html() == x && $('#box4').html() == x && $('#box7').html() == x) ||
+				($('#box2').html() == x && $('#box5').html() == x && $('#box8').html() == x) ||
+				($('#box3').html() == x && $('#box6').html() == x && $('#box6').html() == x) ||
 
-				($('#box1').html() == $x && $('#box5').html() == $x && $('#box9').html() == $x) ||
-				($('#box3').html() == $x && $('#box5').html() == $x && $('#box7').html() == $x) 
+				($('#box1').html() == x && $('#box5').html() == x && $('#box9').html() == x) ||
+				($('#box3').html() == x && $('#box5').html() == x && $('#box7').html() == x) 
 				) {
 					alert('Player 1 Wins!');	
 				}
@@ -41,23 +37,24 @@ $(document).ready( function () {
 				}
 				
 
-			$clickCounter = $clickCounter + 1;
+			clickCounter = clickCounter + 1;
 			$('.playerTurn').html('Player 2 Go!');
 
 
-		}  else {
-			$(this).html($o);
+		}  else if (!$(this).hasClass('X')) {
+			$(this).html(o);
+			$(this).addClass('O');
 			if (
-				($('#box1').html() == $o && $('#box2').html() == $o && $('#box3').html() == $o) ||
-				($('#box4').html() == $o && $('#box5').html() == $o && $('#box6').html() == $o) ||
-				($('#box7').html() == $o && $('#box8').html() == $o && $('#box9').html() == $o) ||
+				($('#box1').html() == o && $('#box2').html() == o && $('#box3').html() == o) ||
+				($('#box4').html() == o && $('#box5').html() == o && $('#box6').html() == o) ||
+				($('#box7').html() == o && $('#box8').html() == o && $('#box9').html() == o) ||
 
-				($('#box1').html() == $o && $('#box4').html() == $o && $('#box7').html() == $o) ||
-				($('#box2').html() == $o && $('#box5').html() == $o && $('#box8').html() == $o) ||
-				($('#box3').html() == $o && $('#box6').html() == $o && $('#box6').html() == $o) ||
+				($('#box1').html() == o && $('#box4').html() == o && $('#box7').html() == o) ||
+				($('#box2').html() == o && $('#box5').html() == o && $('#box8').html() == o) ||
+				($('#box3').html() == o && $('#box6').html() == o && $('#box6').html() == o) ||
 
-				($('#box1').html() == $o && $('#box5').html() == $o && $('#box9').html() == $o) ||
-				($('#box3').html() == $o && $('#box5').html() == $o && $('#box7').html() == $o) 
+				($('#box1').html() == o && $('#box5').html() == o && $('#box9').html() == o) ||
+				($('#box3').html() == o && $('#box5').html() == o && $('#box7').html() == o) 
 				) {
 				alert('Player 2 Wins!');	
 				}
@@ -69,17 +66,22 @@ $(document).ready( function () {
 					alert('Ya Both Win!');
 				} 
 
-			$clickCounter = $clickCounter + 1;
+			clickCounter = clickCounter + 1;
 			$('.playerTurn').html('Player 1 Go!');
-		}
+		} else {
 
+		}
+		console.log(clickCounter);
 	
 		});
+	
 		
 	$(".reset").click( function() {
+		$('.box').removeClass('X O');
 		$('.box').html('');
 		$('.playerTurn').html('Player 1 Go!');
-		$clickCounter = 0;
+		clickCounter = 0;
+		console.log(clickCounter);
 		
 
 
